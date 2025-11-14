70b5fbeccf43bd1ca4629760fa87d8e6af96b895
LLLozer <imtheshittyuser@outlook.com>
2025-11-14 16:34:05 -0300

    Implementada funcionalidad para recargar héroes, obteniendo así otro set de superhéroes random

--- segundo-parcial-tlp2/frontend/src/pages/HomePage.jsx
+++ segundo-parcial-tlp2/frontend/src/pages/HomePage.jsx
@@ -80,9 +80,7 @@ export const HomePage = () => {
 
       <div className="flex justify-center mb-8">
         <button
-          onClick={() => {
-            // TODO: Implementar función para recargar superhéroes
-          }}
+          onClick={getAllHeroes}
           className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded transition-colors"
         >
           Recargar
